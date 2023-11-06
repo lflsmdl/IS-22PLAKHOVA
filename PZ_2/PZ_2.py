9a4c29716f9e366bbbfe8fed14a0c86412b3799a
lflsmdl <lflsmdl@mail.ru>
2023-11-06 18:08:29 +0300

    add tryexcept

--- PZ_2/PZ_2.py
+++ PZ_2/PZ_2.py
@@ -1,4 +1,9 @@
 #С начала суток прошло N секунд (N - целое). Найти количество секунд, прошедших с начала последней минуты
-n = int(input("Введите количетсво секунд: "))
-seconds_since_last_minute = n % 60
-print("С начала последней минуты прошло", seconds_since_last_minute, "секунд")
+while True:
+    try:
+        n = int(input("Введите количетсво секунд: "))
+        seconds_since_last_minute = n % 60
+        print("С начала последней минуты прошло", seconds_since_last_minute, "секунд")
+        break
+    except ValueError:
+        print('Ошибка! Введите целое число')
